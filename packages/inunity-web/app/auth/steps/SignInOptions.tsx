c17300eb80520508509a660a74fbf1d84d55824b
Jean <20massalia@kakao.com>
2024-12-08 18:09:39 +0900

    [feat]: useFunnel 활용하여 온보딩 뷰 구현

---
+++ packages/inunity-web/app/auth/steps/SignInOptions.tsx
@@ -0,0 +1,39 @@
+import { Button, Input, Typography } from "ui";
+
+interface SignInOptionsProps {
+  studentNumber: string;
+  setStudentNumber: (value: string) => void;
+  onNext: () => void;
+  onAttachCertificate: () => void;
+  title: string;
+}
+
+export default function SignInOptions({
+  studentNumber,
+  setStudentNumber,
+  onNext,
+  onAttachCertificate,
+  title,
+}: SignInOptionsProps) {
+  return (
+    <div className="h-dvh flex flex-col mx-5">
+      <Typography variant="HeadingLargeBold" className="mb-4">
+        {title}
+      </Typography>
+      <Input
+        value={studentNumber}
+        setValue={setStudentNumber}
+        placeholder="포탈 아이디"
+        className="mt-6"
+      />
+      <div className="mt-auto mb-5 flex flex-col gap-4">
+        <Button variant="primary" size="large" onClick={onNext}>
+          계속하기
+        </Button>
+        <div className="text-center" onClick={onAttachCertificate}>
+          학교 포탈 계정이 없나요?
+        </div>
+      </div>
+    </div>
+  );
+}
