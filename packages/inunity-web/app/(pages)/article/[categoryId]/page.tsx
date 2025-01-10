2d67d4ebe5031a25ab7620568794627d97426eeb
KimWash <ckm0728wash@gmail.com>
2025-01-10 20:04:11 +0900

    [feat]: 없는 카테고리 핸들

--- packages/inunity-web/app/(pages)/article/[categoryId]/page.tsx
+++ packages/inunity-web/app/(pages)/article/[categoryId]/page.tsx
@@ -1,18 +1,32 @@
-import getDehydratedQuery, { getDehydratedInfiniteQuery } from "@/lib/getDehydratedQuery";
+import getDehydratedQuery, {
+  getDehydratedInfiniteQuery,
+} from "@/lib/getDehydratedQuery";
 import ArticleListContainer from "@/app/(pages)/article/[categoryId]/container";
 import { Hydration } from "@/shared/ui/Hydration";
 import SafeAreaView from "@/widgets/SafeAreaView";
 import { generateMockArticleThumbnails } from "@/entities/article/model/ArticleMock";
 import ArticleQueries from "@/entities/article/hooks/ArticleQueries";
+import CategoryQuery from "@/entities/category/hooks/CategoryQuery";
 
-export default async function Page({ params }: { params: { categoryId: number } }) {
-  const articlesOptions = ArticleQueries.infiniteArticleQuery({categoryId: params.categoryId})
+export default async function Page({
+  params,
+}: {
+  params: { categoryId: number };
+}) {
+  const articlesOptions = ArticleQueries.infiniteArticleQuery({
+    categoryId: params.categoryId,
+  });
   const articlesQuery = await getDehydratedInfiniteQuery(articlesOptions);
+  const categoryQuery = await getDehydratedQuery(CategoryQuery.list());
   return (
-      <SafeAreaView>
+    <SafeAreaView>
+      {categoryQuery.state.data?.find(({ id }) => id == params.categoryId) ? (
         <Hydration queries={[articlesQuery]}>
-        <ArticleListContainer categoryId={params.categoryId}  />
+          <ArticleListContainer categoryId={params.categoryId} />
         </Hydration>
-      </SafeAreaView>
+      ) : (
+        <div>올바르지 않은 카테고리.</div>
+      )}
+    </SafeAreaView>
   );
 }
